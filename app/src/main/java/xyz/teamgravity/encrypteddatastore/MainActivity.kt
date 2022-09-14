0a0d7af0aad04d8c533a9a712d479e08e2892bcc
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-14 12:43:56 +0500

    setup material3

--- app/src/main/java/xyz/teamgravity/encrypteddatastore/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/encrypteddatastore/MainActivity.kt
@@ -4,22 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.encrypteddatastore.ui.theme.EncryptedDataStoreTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EncryptedDataStoreTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
 
                 }
