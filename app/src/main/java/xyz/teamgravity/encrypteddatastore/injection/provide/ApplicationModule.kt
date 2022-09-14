c85daa15ea9244e3c560b3999e64308b2d64f9f1
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-14 14:58:51 +0500

    provide dependencies

--- app/src/main/java/xyz/teamgravity/encrypteddatastore/injection/provide/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/encrypteddatastore/injection/provide/ApplicationModule.kt
@@ -1,10 +1,13 @@
 package xyz.teamgravity.encrypteddatastore.injection.provide
 
+import android.app.Application
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import xyz.teamgravity.encrypteddatastore.core.util.CryptoManager
+import xyz.teamgravity.encrypteddatastore.data.local.preferences.Preferences
+import xyz.teamgravity.encrypteddatastore.data.local.preferences.PreferencesSerializer
 import java.security.KeyStore
 import javax.inject.Singleton
 
@@ -21,4 +24,16 @@ object ApplicationModule {
     @Provides
     @Singleton
     fun provideCryptoManager(keyStore: KeyStore): CryptoManager = CryptoManager(keyStore)
+
+    @Provides
+    @Singleton
+    fun providePreferencesSerializer(cryptoManager: CryptoManager): PreferencesSerializer = PreferencesSerializer(cryptoManager)
+
+    @Provides
+    @Singleton
+    fun providePreferences(application: Application, preferencesSerializer: PreferencesSerializer): Preferences =
+        Preferences(
+            context = application,
+            serializer = preferencesSerializer
+        )
 }
